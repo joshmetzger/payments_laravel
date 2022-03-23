a14444bb00713b8fe0fc0a85266f5ddc2ffead5c
vagrant <vagrant@payments-laravel>
2022-03-23 03:26:20 +0000

    layouts and app updated for erros and success messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Welcome') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    
-                    This is a payments processing system using the mnost popular platforms
+                  
+
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
